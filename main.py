27525411d300859bd2d0759d74a6f9c609c1e18f
Eirik-eu <github.com.earthworm256@passmail.com>
2025-07-29 01:13:52 +0200

    added a player sprite, inheriting from a circleshape

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while pygame.get_init:
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, 000000)
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
+        
+        # sets fps to 60
+        dt = clock.tick(60) / 1000
 
     
-
-
 if __name__ == "__main__":
     main()
